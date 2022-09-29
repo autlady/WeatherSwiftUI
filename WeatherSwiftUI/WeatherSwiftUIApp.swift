45f3ca84872f063ceea229e473e1a2457d347209
Julia Grigoreva <julia_panina@mail.ru>
2022-09-29 20:08:25 +0300

    Initial commit

---
+++ WeatherSwiftUI/WeatherSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherSwiftUIApp.swift
+//  WeatherSwiftUI
+//
+//  Created by  Юлия Григорьева on 15.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct WeatherSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
